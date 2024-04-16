dcca2de1fd35f61066db5d5acad422e63d7ae24b
federicaarosio <arosio.fede@gmail.com>
2024-04-16 22:17:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php cms</title>
+    <!-- Custom css -->
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+    <?php include './partials/header.php' ?>
+    <main>
+        <h1>
+            Main
+        </h1>
+    </main>
+    <?php include './partials/footer.php' ?>
+</body>
+
+
+</html>
